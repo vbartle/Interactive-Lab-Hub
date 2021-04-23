13a209978ae54b7ab39ee3f64480eb676f4e2002
vince <vince@bartle.io>
2021-04-23 10:38:28 -0400

    final

--- Lab 5/run_cam.py
+++ Lab 5/run_cam.py
@@ -8,10 +8,18 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    #mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
